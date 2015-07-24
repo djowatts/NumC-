fafe9045cacfccbb1e3281c737e3552e7fc2189e
David Watts <dwatts@footclicks.com>
2015-07-24 20:56:35 +0100

    Initial work on N Dimensional Arrays

---
+++ NumCSharp.Domain.Tests/NdArray/NdArrayTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace NumCSharp.Domain.Tests.NdArray
+{
+    public class NdArrayTests
+    {
+        [Fact]
+        public void GeneratingAnIntNdArrayOfZeros()
+        {
+            var arr = NumCSharp.Domain.NdArray<int>.OfZeros(1, 3);
+
+            Assert.True(arr.GetValue(0) is int);
+            Assert.True(arr.Length == 3);
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                Assert.Equal(0, arr.GetValue(i));
+            }
+        }
+    }
+}
---
+++ NumCSharp.Domain/NdArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumCSharp.Domain
+{
+    public static class NdArray<T> where T : IComparable<T>, new()
+    {
+        public static Array OfZeros(int rows, int columns)
+        {
+            if (rows == 1)
+            {
+                return new T[columns];
+            }
+            return new T[rows, columns];
+        }
+    }
+}
